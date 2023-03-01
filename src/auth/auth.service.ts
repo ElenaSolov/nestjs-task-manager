de664c6b39f928406b1815772b43e2169c71bb0f
Elena Soloveva <seaver@yandex.ru>
2023-03-01 14:16:03 +0200

    Feat: add auth module and signup route and functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,11 @@ export class AuthService {
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+  }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,10 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: string;
 
+  @IsNotEmpty()
   @Column()
   username: string;
 
