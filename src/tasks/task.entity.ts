2583d8d9aac5c028116071c9e3c35e5d2270856d
Elena Soloveva <seaver@yandex.ru>
2023-03-02 15:54:22 +0200

    Feat: serialize user data

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { UserEntity } from '../auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class TaskEntity {
@@ -16,6 +17,8 @@ export class TaskEntity {
   @Column()
   status: TaskStatus;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @ManyToOne((_type) => UserEntity, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: UserEntity;
 }
